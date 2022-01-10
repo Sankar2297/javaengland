befa07b5c8bded5e7718f27b76958301590d6ba3
sankar2297 <sankar22raj@gmail.com>
2022-01-10 12:39:43 -0800

    javaengland

---
+++ src/org/eng/Depart.java
@@ -0,0 +1,15 @@
+package org.eng;
+
+public class Depart {
+	
+	private void method() {
+		System.out.println("data");
+		
+		
+
+	}
+public static void main(String[] args) {
+	Depart d = new Depart();
+	d.method();
+}
+}
